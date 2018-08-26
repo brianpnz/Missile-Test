da8926cebb8b4ff342c3d6e73803618d01c0bb86
Brian Payne <brianp.nz@gmail.com>
2018-08-26 15:12:30 +1200

    First commit
    First commit of the basic missile test

---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+    //private GameObject selectedEnemy;
+    private string selectedEnemy;
+    private EnemyScript enemyInstance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateSelectedEnemy(EnemyScript enemy)
+    {
+        enemyInstance = enemy;
+
+        Debug.Log(enemyInstance.name + " was selected");
+        
+    }
+
+    public EnemyScript GetSelectedEnemy()
+    {
+            return enemyInstance;
+    }
+
+
+}
---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    private ControllerScript controller;
+
+    private void Awake()
+    {
+        controller = GameObject.FindObjectOfType<ControllerScript>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        controller.UpdateSelectedEnemy(this);
+    }
+}
